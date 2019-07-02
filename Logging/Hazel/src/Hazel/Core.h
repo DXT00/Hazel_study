0ee6bc9a4738ae039f8664734c2fbd5b3522a614
DXT00 <dingxiaotong11@163.com>
2019-07-02 10:33:32 +0800

    Logging

---
+++ Logging/Hazel/src/Hazel/Core.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef HZ_PLATFORM_WINDOWS
+	#ifdef HZ_BUILD_DLL
+		#define HAZEL_API __declspec(dllexport)
+	#else
+		#define HAZEL_API __declspec(dllimport)
+	#endif
+
+#else 
+	#error Hazel only support windows!
+#endif
